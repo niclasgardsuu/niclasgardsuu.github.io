7db02e76193f8fd925400f0231d8c86a2a36f8ae
Jakob Paulsson <jakob.paulsson123@gmail.com>
2023-11-20 17:10:04 +0100

    meme

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
 const weekContainers = document.getElementsByClassName("week-container");
-const descriptions = ["Made some changes to the progress website. We added three different subpages, one for Home, Progress, and Group Members.<br><br>We have looked at different data sets for recipes and ingredients and found one that contains ingredients, cook time, ratings, and images.<br><br>We also created a sketch for the recipe application in Figma.",
-                      "We initialized the progress website."]
+const descriptions = ["During this week, we started to implement the application based on the sketches done in Figma.<br><br>We have implemented most of the recipes and ingredients pages, and added stub-pages for the dashboard and account page, aswell as a functionality for searching for recipes.<br><br>For the database, we chose to use SQLite as our driver together with express.js for integration with our application.<br><br>Furthermore, we implemented get and set queries for recipes, ingredients and searching.<br><br>Lastly, we implemented a E2E testing suite using Cypress.",
+  "Made some changes to the progress website. We added three different subpages, one for Home, Progress, and Group Members.<br><br>We have looked at different data sets for recipes and ingredients and found one that contains ingredients, cook time, ratings, and images.<br><br>We also created a sketch for the recipe application in Figma.",
+  "We initialized the progress website."]
 let currentWeek = 44 + descriptions.length - 1;
 
 for (const description of descriptions) {
   const weekContainer = document.createElement("div");
   weekContainer.className = "week-container";
-  
+
   const leftContainer = document.createElement("div");
   leftContainer.className = "left-container";
-  
+
   const week = document.createElement("button");
   week.className = "week";
   week.innerHTML = "V" + currentWeek;
-  
+
   const dottedLine = document.createElement("div");
   dottedLine.className = "dotted-line";
 
   const line = document.createElement("vl");
   line.className = "line closed";
-  
+
   const weekDescription = document.createElement("div");
   weekDescription.className = "week-description closed";
   weekDescription.innerHTML = description
